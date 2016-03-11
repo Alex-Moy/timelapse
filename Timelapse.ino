3d85b31b75043cdf18d31e29964685fa7ab90558
Alex-Moy <electroalexdu26@gmail.com>
2016-03-11 21:52:57 +0100

    Update Timelapse.ino

--- Timelapse.ino
+++ Timelapse.ino
@@ -2,74 +2,93 @@
 #include <Stepper.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
-Stepper moteur(48, 8, 9, 10, 11);
+Stepper moteur(48, A0, A1, A2, A3);
 
-const int btvalid = 0;
-const int bthaut = 0;
-const int btbas = 0;
+const int btvalid = 8;
+const int bthaut = 9;
+const int btbas = 10;
 const int led = 13;
 
-byte heure = 0;
-byte minutes = 0;
-byte angle = 0;
+byte heure;
+byte minutes;
+int angle;
 
-byte vitesse = 0;
+int vitesse;
 
 unsigned long intervale;
 
-byte mode = 0;
-byte indicedirection = -1;
-boolean valid = false;
+byte mode = 1;
+int indicedirection;
+boolean valid;
 
 int debounce = 300;
+int affichage = 60;
+unsigned long lasttime = 0;
+
+byte mycaracteredegre[8]={B00100,B01010,B00100,B00000,B00000,B00000,B00000,B00000};
+
+byte mycaractereeaccent[8]={B00010,B00100,B01110,B10001,B11111,B10000,B01110,B00000};
 
 void setup() {
   lcd.begin(16,2);
-  moteur.setSpeed(90);
+  Serial.begin(9600);
+  moteur.setSpeed(60);
 
   pinMode(btvalid, INPUT_PULLUP);
    pinMode(bthaut, INPUT_PULLUP);
     pinMode(btbas, INPUT_PULLUP);
      pinMode(led, OUTPUT);
+
+  lcd.createChar(0, mycaracteredegre);
+  lcd.createChar(1, mycaractereeaccent);
 }
 
 void loop() {
-  selectmode(); //OK
-  if(mode == 0) {modetempsangle(); selectdirection(); affichetempsangle(); validation();}
-  if(mode == 1) {modevitesse(); selectdirection(); affichevitesse(); validation();}
-  if(mode == 2) {manuel();} //OK
+  selectmode();
+  if(mode == 1) {modetempsangle(); selectdirection(); affichetempsangle(); validation();}
+  if(mode == 2) {modevitesse(); selectdirection(); affichevitesse(); validation();}
+  if(mode == 3) {manuel();}
 }
 
-void selectmode()
+void selectmode() //Testé OK
 {
-  while(digitalRead(btvalid) == 1) {
-    lcd.clear();
+  lcd.clear();
     
-    lcd.setCursor(0,0);
-    lcd.print("Mode:");
+  lcd.setCursor(0,0);
+  lcd.print("Mode:");
   
+  while(digitalRead(btvalid) == 1) {
     lcd.setCursor(0,1);
-    if(mode == 0) {lcd.print("Temps et angle");}
-    if(mode == 1) {lcd.print("Temps par degre");}
-    if(mode == 2) {lcd.print("Manuel");}
+    if(mode == 1) {lcd.print("Temps et angle ");}
+    if(mode == 2) {lcd.print("Temps par degre");}
+    if(mode == 3) {lcd.print("Manuel         ");}
 
     if(digitalRead(bthaut) == 0) {
-      mode++;
+      mode--;
+      allumeled();
       delay(debounce);
     }
     if(digitalRead(btbas) == 0) {
-      mode--;
+      mode++;
+      allumeled();
       delay(debounce);
     }
 
-    if(mode < 0) {mode = 2;}
-    if(mode > 2) {mode = 0;}
+    if(mode < 1) {mode = 3;}
+    if(mode > 3) {mode = 1;}
+
+    delay(affichage);
   }
+  allumeled();
   delay(debounce);
 }
 
-void modetempsangle()
+void modetempsangle() //Testé OK
 {
+  heure = 0;
+  minutes = 0;
+  angle = 0;
+  
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(" Temps:  Angle :");
@@ -77,7 +96,7 @@ void modetempsangle()
   lcd.setCursor(0,1);
   lcd.print(" 00h00m   000");
   lcd.setCursor(13,1);
-  lcd.print((char)223); //Signe °
+  lcd.write((byte)0); //Signe °
 
   while(digitalRead(btvalid) == 1) {
     lcd.setCursor(1,1);
@@ -87,6 +106,7 @@ void modetempsangle()
       else {heure = heure - 10;}
       lcd.print(heure / 10);
       allumeled();
+      lcd.setCursor(1,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -94,9 +114,11 @@ void modetempsangle()
       else {heure = heure + 10;}
       lcd.print(heure / 10);
       allumeled();
+      lcd.setCursor(1,1);
       delay(debounce);
     }
   }
+  allumeled();
   delay(debounce);
 
   while(digitalRead(btvalid) == 1) {
@@ -107,6 +129,7 @@ void modetempsangle()
       else {heure = heure - 1;}
       lcd.print(heure % 10);
       allumeled();
+      lcd.setCursor(2,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -114,29 +137,34 @@ void modetempsangle()
       else {heure = heure + 1;}
       lcd.print(heure % 10);
       allumeled();
+      lcd.setCursor(2,1);
       delay(debounce);
     }
   }
+  allumeled();
   delay(debounce);
 
   while(digitalRead(btvalid) == 1) {
     lcd.setCursor(4,1);
     lcd.blink();
     if(digitalRead(btbas) == 0) {
-      if((minutes / 10) == 0) {minutes = 90;}
+      if((minutes / 10) == 0) {minutes = 50;}
       else {minutes = minutes - 10;}
       lcd.print(minutes / 10);
       allumeled();
+      lcd.setCursor(4,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
-      if((minutes / 10) == 9) {minutes = 0;}
+      if((minutes / 10) == 5) {minutes = 0;}
       else {minutes = minutes + 10;}
       lcd.print(minutes / 10);
       allumeled();
+      lcd.setCursor(4,1);
       delay(debounce);
     }
   }
+  allumeled();
   delay(debounce);
 
   while(digitalRead(btvalid) == 1) {
@@ -147,6 +175,7 @@ void modetempsangle()
       else {minutes = minutes - 1;}
       lcd.print(minutes % 10);
       allumeled();
+      lcd.setCursor(5,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -154,9 +183,11 @@ void modetempsangle()
       else {minutes = minutes + 1;}
       lcd.print(minutes % 10);
       allumeled();
+      lcd.setCursor(5,1);
       delay(debounce);
     }
   }
+  allumeled();
   delay(debounce);
 
 
@@ -169,6 +200,7 @@ void modetempsangle()
       else {angle = angle - 100;}
       lcd.print(angle / 100);
       allumeled();
+      lcd.setCursor(10,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -176,9 +208,11 @@ void modetempsangle()
       else {angle = angle + 100;}
       lcd.print(angle / 100);
       allumeled();
+      lcd.setCursor(10,1);
       delay(debounce);
     }
   }
+  allumeled();
   delay(debounce);
 
   while(digitalRead(btvalid) == 1) {
@@ -189,6 +223,7 @@ void modetempsangle()
       else {angle = angle - 10;}
       lcd.print((angle % 100) / 10);
       allumeled();
+      lcd.setCursor(11,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -196,9 +231,11 @@ void modetempsangle()
       else {angle = angle + 10;}
       lcd.print((angle % 100) / 10);
       allumeled();
+      lcd.setCursor(11,1);
       delay(debounce);
     }
   }
+  allumeled();
   delay(debounce);
 
   while(digitalRead(btvalid) == 1) {
@@ -209,6 +246,7 @@ void modetempsangle()
       else {angle = angle - 1;}
       lcd.print(angle % 10);
       allumeled();
+      lcd.setCursor(12,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -216,44 +254,46 @@ void modetempsangle()
       else {angle = angle + 1;}
       lcd.print(angle % 10);
       allumeled();
+      lcd.setCursor(12,1);
       delay(debounce);
     }
   }
+  allumeled();
   lcd.noBlink();
   delay(debounce);
 
-  intervale = (((heure * 60) + minutes) * 60 * 1000) / (angle * 8);
+  intervale = (((heure * 60) + minutes) * 60000) / (angle * 8);
 }
 
 void affichetempsangle()
 {
   lcd.clear();
   lcd.setCursor(1,0);
+  if(heure < 10) {lcd.print("0");}
   lcd.print(heure);
-  lcd.setCursor(3,0);
   lcd.print("h");
-  lcd.setCursor(4,0);
+  if(heure < 10) {lcd.print("0");}
   lcd.print(minutes);
-  lcd.setCursor(6,0);
   lcd.print("m");
 
-  lcd.setCursor(10,0);
+  lcd.setCursor(11,0);
+  if(angle < 10) {lcd.print("0");}
+  if(angle < 100) {lcd.print("0");}
   lcd.print(angle);
-  lcd.setCursor(13,0);
-  lcd.print((char)223);
+  lcd.write((byte)0);
 }
 
 void modevitesse()
 {
+  vitesse = 0;
+  
   lcd.clear();
 
   lcd.setCursor(0,0);
   lcd.print(" Temps par pas: ");
   
-  lcd.setCursor(4,1);
-  lcd.print(",");
-  lcd.setCursor(7,1);
-  lcd.print("s/pas");
+  lcd.setCursor(3,1);
+  lcd.print("0,00s/pas");
   
   while(digitalRead(btvalid) == 1) {
     lcd.setCursor(3,1);
@@ -263,6 +303,7 @@ void modevitesse()
       else {vitesse = vitesse - 100;}
       lcd.print(vitesse / 100);
       allumeled();
+      lcd.setCursor(3,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -270,6 +311,7 @@ void modevitesse()
       else {vitesse = vitesse + 100;}
       lcd.print(vitesse / 100);
       allumeled();
+      lcd.setCursor(3,1);
       delay(debounce);
     }
   }
@@ -283,6 +325,7 @@ void modevitesse()
       else {vitesse = vitesse - 10;}
       lcd.print((vitesse % 100) / 10);
       allumeled();
+      lcd.setCursor(5,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -290,6 +333,7 @@ void modevitesse()
       else {vitesse = vitesse + 10;}
       lcd.print((vitesse % 100) / 10);
       allumeled();
+      lcd.setCursor(5,1);
       delay(debounce);
     }
   }
@@ -303,6 +347,7 @@ void modevitesse()
       else {vitesse = vitesse - 1;}
       lcd.print(vitesse % 10);
       allumeled();
+      lcd.setCursor(6,1);
       delay(debounce);
     }
     if(digitalRead(bthaut) == 0) {
@@ -310,13 +355,14 @@ void modevitesse()
       else {vitesse = vitesse + 1;}
       lcd.print(angle % 10);
       allumeled();
+      lcd.setCursor(6,1);
       delay(debounce);
     }
   }
   lcd.noBlink();
   delay(debounce);
 
-  intervale = vitesse * 100;
+  intervale = vitesse * 10;
 }
 
 void affichevitesse()
@@ -332,54 +378,62 @@ void affichevitesse()
   lcd.print("s/pas");
 }
 
-void selectdirection()
+void selectdirection() //Testé OK
 {
+  indicedirection = -1;
+  
   lcd.clear();
     
   lcd.setCursor(0,0);
   lcd.print("Direction:");
   
   while(digitalRead(btvalid) == 1) {
-    lcd.clear();
-    
     lcd.setCursor(0,1);
     if(indicedirection == -1) {lcd.print("Vers la gauche");}
     if(indicedirection == 1) {lcd.print("Vers la droite");}
 
     if(digitalRead(bthaut) == 0) {
       indicedirection = indicedirection + 2;
+      allumeled();
       delay(debounce);
     }
     if(digitalRead(btbas) == 0) {
       indicedirection = indicedirection - 2;
+      allumeled();
       delay(debounce);
     }
     
-    if(mode < -1) {mode = 1;}
-    if(mode > 1) {mode = -1;}
+    if(indicedirection < -1) {indicedirection = 1;}
+    if(indicedirection > 1) {indicedirection = -1;}
+
+    delay(affichage);
   }
+  allumeled();
   delay(debounce);
 }
 
 void validation()
 {
-  lcd.setCursor(0,1);
+  valid = false;
+  lcd.setCursor(2,1);
   lcd.print("Valider?");
-  lcd.setCursor(10,1);
 
   while(digitalRead(btvalid) == 1) {
+    lcd.setCursor(11,1);
     if(valid == false) {lcd.print("NON");}
     if(valid == true) {lcd.print("OUI");}
 
     if(digitalRead(bthaut) == 0 || digitalRead(btbas) == 0) {
       //Synthaxe opérateur conditionnel : "Condition?Instruction1:Instruction2"
       valid = (valid == false ? true : false);
+      allumeled();
       delay(debounce);
     }
   }
-  if(valid == true) {
-    execution();
-  }
+  allumeled();
+  delay(debounce);
+  if(valid == true) {execution();}
+  else {annulation();}
 }
 
 void execution()
@@ -390,10 +444,25 @@ void execution()
   lcd.setCursor(0,1);
   lcd.print("      ...       ");
   while(digitalRead(btvalid) == 1) {
-    moteur.step(indicedirection);
-    allumeled();
-    delay(intervale);
+    if((millis() - lasttime) >= intervale) {
+      lasttime = millis();
+      moteur.step(indicedirection);
+      allumeled();
+    }
   }
+  allumeled();
+  delay(debounce);
+}
+
+void annulation()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Vous avez annul");
+  lcd.write((byte)1);
+  while(digitalRead(btvalid) == 1) {/* Vide */}
+  allumeled();
+  delay(debounce);
 }
 
 void manuel()
